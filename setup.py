f140ed098e9fb6c3fbf63435a0a3089fbac2e75a
Benned-H <benned1@gmail.com>
2025-05-06 02:11:00 -0400

    Make CUDA extension build

--- setup.py
+++ setup.py
@@ -3,32 +3,44 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
 
-from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    packages=find_packages(include=["diff_gaussian_rasterization*"]),
+    install_requires=["torch"],
     ext_modules=[
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp",
+            ],
+            extra_compile_args={
+                "cxx": ["-std=c++17"],  # Host compiler
+                "nvcc": [
+                    "-std=c++17",
+                    "-Wno-deprecated-declarations",
+                    "-Ithird_party/glm/",
+                ],  # NVCC
+            },
+        ),
+    ],
     cmdclass={
-        'build_ext': BuildExtension
-    }
+        "build_ext": BuildExtension,
+    },
 )
